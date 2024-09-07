9b5a9d9c2f911abbd0cc5127908f7602e071b7b7
javierni <jnog@unizar.es>
2024-09-07 17:52:58 +0200

    Add index/search demo with whoosh library.

---
+++ woosh_demo/index.py
@@ -0,0 +1,75 @@
+"""
+index.py
+Author: Javier Nogueras Iso
+Last update: 2024-09-07
+
+Simple program to create an inverted index with the contents of text/xml files contained in a docs folder
+This program is based on the whoosh library. See https://whoosh.readthedocs.io/en/latest/index.html .
+Usage: python index.py -index <index folder> -docs <docs folder>
+"""
+
+from whoosh.index import create_in
+from whoosh.fields import *
+
+import os
+
+import xml.etree.ElementTree as ET
+
+def create_folder(folder_name):
+    if (not os.path.exists(folder_name)):
+        os.mkdir(folder_name)
+
+class MyIndex:
+    def __init__(self,index_folder):
+        schema = Schema(path=ID(stored=True), content=TEXT)
+        create_folder(index_folder)
+        index = create_in(index_folder, schema)
+        self.writer = index.writer()
+
+    def index_docs(self,docs_folder):
+        if (os.path.exists(docs_folder)):
+            for file in sorted(os.listdir(docs_folder)):
+                # print(file)
+                if file.endswith('.xml'):
+                    self.index_xml_doc(docs_folder, file)
+                elif file.endswith('.txt'):
+                    self.index_txt_doc(docs_folder, file)
+        self.writer.commit()
+
+    def index_txt_doc(self, foldername,filename):
+        file_path = os.path.join(foldername, filename)
+        print(file_path)
+        with open(file_path) as fp:
+            text = ' '.join(line for line in fp if line)
+        print(text)
+        self.writer.add_document(path=filename, content=text)
+
+    def index_xml_doc(self, foldername, filename):
+        file_path = os.path.join(foldername, filename)
+        # print(file_path)
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        raw_text = "".join(root.itertext())
+        # break into lines and remove leading and trailing space on each
+        text = ' '.join(line.strip() for line in raw_text.splitlines() if line)
+        # print(text)
+        self.writer.add_document(path=filename,content=text)
+
+if __name__ == '__main__':
+
+    index_folder = '../whooshindex'
+    docs_folder = '../docs'
+    i = 1
+    while i < len(sys.argv):
+        if sys.argv[i] == '-index':
+            index_folder = sys.argv[i + 1]
+            i = i + 1
+        elif sys.argv[i] == '-docs':
+            docs_folder = sys.argv[i + 1]
+            i = i + 1
+        i = i + 1
+
+    my_index = MyIndex(index_folder)
+    my_index.index_docs(docs_folder)
+
+
---
+++ woosh_demo/search.py
@@ -0,0 +1,53 @@
+"""
+search.py
+Author: Javier Nogueras Iso
+Last update: 2024-09-07
+
+Program to search a free text query on a previously created inverted index.
+This program is based on the whoosh library. See https://whoosh.readthedocs.io/en/latest/index.html .
+Usage: python search.py -index <index folder>
+"""
+
+import sys
+
+from whoosh.qparser import QueryParser
+from whoosh import scoring
+import whoosh.index as index
+
+class MySearcher:
+    def __init__(self, index_folder, model_type = 'tfidf'):
+        ix = index.open_dir(index_folder)
+        if model_type == 'tfidf':
+            # Apply a vector retrieval model as default
+            self.searcher = ix.searcher(weighting=scoring.TF_IDF())
+        else:
+            # Apply the probabilistic BM25F model, the default model in searcher method
+            self.searcher = ix.searcher()
+        self.parser = QueryParser("content", ix.schema)
+
+    def search(self, query_text):
+        query = self.parser.parse(query_text)
+        results = self.searcher.search(query)
+        print('Returned documents:')
+        i = 1
+        for result in results:
+            print(f'{i} - File path: {result.get("path")}, Similarity score: {result.score}')
+            i += 1
+
+if __name__ == '__main__':
+    index_folder = '../whooshindex'
+    language = 'spanish'
+    i = 1
+    while (i < len(sys.argv)):
+        if sys.argv[i] == '-index':
+            index_folder = sys.argv[i+1]
+            i = i + 1
+        i = i + 1
+
+    searcher = MySearcher(index_folder, language)
+
+    #query = 'first'
+    query = input('Introduce a query: ')
+    while query != 'q':
+        searcher.search(query)
+        query = input('Introduce a query (\'q\' for exit): ')
